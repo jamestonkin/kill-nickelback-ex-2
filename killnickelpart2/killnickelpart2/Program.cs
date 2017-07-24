67b217beb360708dab960d65f8aed51070800f78
James W. Tonkin <btonkin71@comcast.net>
2017-07-24 08:53:46 -0500

    Exercise Complete

--- killnickelpart2/killnickelpart2/Program.cs
+++ killnickelpart2/killnickelpart2/Program.cs
@@ -10,6 +10,31 @@ namespace killnickelpart2
     {
         static void Main(string[] args)
         {
+            List<(string artistName, string songTitle)> goodSongs = new List<(string, string)>();
+            HashSet<(string, string)> allSongs = new HashSet<(string, string)>();
+
+            allSongs.Add(("Nickelback", "How You Remind Me"));
+            allSongs.Add(("Elmo", "Elmo's Got the Moves"));
+            allSongs.Add(("Weird Al", "I'm Fat"));
+            allSongs.Add(("Nickelback", "Rockstar"));
+            allSongs.Add(("Nickelback", "Photograph"));
+            allSongs.Add(("Taylor Swift", "Fearless"));
+            allSongs.Add(("George Jones", "He Stopped Loving Her Today"));
+
+            foreach ((string artistName, string songTitle) song in allSongs)
+            {
+                if(song.artistName != "Nickelback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+
+            foreach((string artistName, string songTitle) songs in goodSongs)
+            {
+                Console.WriteLine($"\n{songs.artistName} - {songs.songTitle}");
+            }
+
+            Console.Read();
         }
     }
 }
